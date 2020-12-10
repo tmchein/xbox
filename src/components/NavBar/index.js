43799721a5c16066143ce0c44ceb954e3ca98797
tmchein <davidr2998@gmail.com>
2020-12-09 23:40:22 -0500

    First commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </nav>
+  );
+};
+
+export default NavBar;
